ba2d97ebd2c5a136972c60eebe3634b7fe25875c
user128830 <user128830@ny212.local>
2017-08-31 16:18:52 -0700

    game is done according to the tutorial

--- ApplePie/Game.swift
+++ ApplePie/Game.swift
@@ -13,4 +13,35 @@ struct Game {
     var word: String
     
     var incorrectMovesRemaining: Int
+    
+    // array of selected letters for current game
+    var guessedLetters: [Character]
+    
+    mutating func playerGuessed(letter: Character) {
+        // appending the array of chosen letters
+        guessedLetters.append(letter)
+        
+        // decreasing the number of attampts for user in case of wrong letter
+        if !word.characters.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+    }
+    
+    // computed property of 
+    var formattedWord: String {
+        
+        var guessedWord = ""
+        
+        for letter in word.characters {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += "*"
+            }
+        }
+        print(guessedWord)
+        return guessedWord
+    }
+    
+    
 }
--- ApplePie/ViewController.swift
+++ ApplePie/ViewController.swift
@@ -20,48 +20,107 @@ class ViewController: UIViewController {
     // number between 1 and 7 because of number available images of the tree
     let incorrectMovesAlowed = 7
     
-    // number of wins and losses for most down label
-    var totalWins = 0
-    var totalLosses = 0
+    // number of wins and losses for score label
+    var totalWins = 0 {
+        didSet {
+            newRound()
+        }
+    }
+    var totalLosses = 0 {
+        didSet {
+            newRound()
+        }
+    }
+    
+    // instance of the Game struct that keeping information about current game
+    var currentGame: Game!
     
     
-    
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        print("Hello World!")
+    // updates labels every newRound()
+    func updateUI() {
         
-        // instance of the Game struct that keeping information about current game
-        var currentGame: Game!
+        //adding spaces between letter in "word label" using computed property
+        var letters = [String]()
+        for letter in currentGame.formattedWord.characters {
+                letters.append(String(letter))
+            }
+        let wordWithSpacing = letters.joined(separator: " ")
+
+        correctWordLabel.text = wordWithSpacing
         
-        func newRound() {
+        // updating score label
+        scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
+        
+        // updating image view
+        treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
+        
+        
+    }
+    
+    func updateGameState() {
+        if currentGame.incorrectMovesRemaining == 0 {
+            totalLosses += 1
+        } else if currentGame.word == currentGame.formattedWord {
+            totalWins += 1
+        } else {
+            updateUI()
+        }
+        
+    }
+    
+    func enableLetterButton(_ enable: Bool) {
+        for button in letterButton {
+            button.isEnabled = enable
+        }
+    }
+    
+    // everything that should be done when new game is starting
+    func newRound() {
+        
+        if !listOfWords.isEmpty {
             // capturing the first word from array
-            print(listOfWords)
             let newWord = listOfWords.removeFirst()
-            print(listOfWords)
             
             // intializing the instance
-            currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAlowed)
+            currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAlowed, guessedLetters: [])
             
+            // before new round all buttons should be enabled
+            enableLetterButton(true)
             
+            // calling updateUI() every round
+            updateUI()
+        } else {
+            enableLetterButton(false)
         }
+
         
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
         newRound()
-        
-        func updateUI() {
-            scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
-            treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
-        }
-        
-        updateUI()
-        
+
     }
 
     
     @IBAction func buttonTapped(_ sender: UIButton) {
         sender.isEnabled = false
+        
+        let letterString = sender.title(for: .normal)!
+        let letter = Character(letterString.lowercased())
+        currentGame.playerGuessed(letter: letter)
+        updateUI()
+        
+        // is this the best place to run this method?
+        updateGameState()
+    
+        print(letter)
+        print(currentGame)
+        
     }
+    
+    
 
 
 }
